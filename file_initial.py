6f383680f361b3a3326eca862b13fbb75aafdc1d
Kenichiro-bit <82493142+Kenichiro-bit@users.noreply.github.com>
2021-11-24 18:04:22 +0900

    Create file_initial.py

---
+++ file_initial.py
@@ -0,0 +1,11 @@
+## ファイル名とシート名を定義
+## originalのファイル
+excel_file1 = 'D:/UserArea/J0134661/Desktop/便利機能/excel_check/excel_check/コピーFMEA試験正解表_次世代PDRV_OBD対象⊿8_masuo.xlsx'
+excel_sheet1 = 'FMEA_PDRV_Sheet_益尾確認'
+
+## 比較用のファイル
+excel_file2 = 'D:/UserArea/J0134661/Desktop/便利機能/excel_check/excel_check/コピーFMEA試験正解表_次世代PDRV_OBD対象⊿8_masuo.xlsx'
+excel_sheet2 = 'FMEA_PDRV_Sheet_益尾確認'
+
+get_col_num_checking = int(input('比較したい列番号を入力してください'))
+get_col_num_original = int(input('参照したい元の列番号を入力してください'))
